3eb9d9cf5074b8d659cea55b058bf7e248c24ce5
Clayton Santos <claytondesousasantos@gmail.com>
2021-10-24 21:15:26 -0300

    Projeto Concluido

---
+++ src/app/home/novo-usuario/minusculo.validator.ts
@@ -0,0 +1,10 @@
+import { AbstractControl } from "@angular/forms";
+
+export function minusculoValidator(control:AbstractControl) {
+    const valor = control.value as string;
+    if(valor !== valor.toLocaleUpperCase()) {
+        return {minusculo: true};
+    } else {
+        return null;
+    }
+};
---
+++ src/app/home/novo-usuario/novo-usuario.ts
@@ -0,0 +1,6 @@
+export interface NovoUsuario {
+    useName:string;
+    email:string;
+    fullName: string;
+    password:string;
+}
